c417227675825bddaaec2107f29c5af511453ba6
AzizSalimov <122611579+AzizSalimov@users.noreply.github.com>
2023-03-31 08:48:53 +0500

    Add files via upload

---
+++ Django_part1_part2/polls/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+
+# Create your models here.
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published', null=True)
+
+    def __str__(self):
+        return self.question_text
+
+class Choise(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.PROTECT)
+    choise_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+    is_true = models.BooleanField(default=False)
+
+
+    def __str__(self):
+        return f"{self.question} - {self.choise_text} - {self.is_true}"
