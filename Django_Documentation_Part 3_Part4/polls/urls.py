f2c8a5a83eda003e1aae634d35eb7ee988e92375
AzizSalimov <122611579+AzizSalimov@users.noreply.github.com>
2023-04-03 09:32:32 +0500

    Django

---
+++ Django_Documentation_Part 3_Part4/polls/urls.py
@@ -0,0 +1,28 @@
+from django.urls import path
+
+from polls.views import homepage, questions_lists, question_detail
+
+app_name = 'polls'
+
+urlpatterns = [
+    path('', homepage, name='homepage'),
+    path('questions/', questions_lists, name='questions_lists'),
+    path('questions/<int:pk>/', question_detail, name='question_detail')
+]
+
+# from django.urls import path
+#
+# from polls.views import homepage, questions_list, questions_detail
+#
+# app_name = 'polls'
+#
+# urlpatterns = [
+#     path('information/', homepage, name='homepage'),
+#
+#     path('questions/', questions_list, name = 'questions_list'),
+#
+#     path('questions/<int:pk>/', questions_detail, name = 'questionsde_detail')
+#
+#
+# ]
+
